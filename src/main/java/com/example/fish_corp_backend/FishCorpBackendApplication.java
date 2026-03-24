be8635dca37cdddf34b3ab1af52bdb451dbde214
Jøran <jorancj@gmail.com>
2026-03-24 16:55:22 +0100

    initial commit

---
+++ src/main/java/com/example/fish_corp_backend/FishCorpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fish_corp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishCorpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishCorpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fish_corp_backend/FishCorpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fish_corp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishCorpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
